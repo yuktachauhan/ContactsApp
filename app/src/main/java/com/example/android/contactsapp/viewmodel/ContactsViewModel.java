d7449e393c56bf51773017985947cda7a4ffa8ec
yuktachauhan <yuktach20167@gmail.com>
2019-07-01 03:34:57 +0530

    contacts viewmodel created

--- app/src/main/java/com/example/android/contactsapp/viewmodel/ContactsViewModel.java
+++ app/src/main/java/com/example/android/contactsapp/viewmodel/ContactsViewModel.java
@@ -1,6 +1,25 @@
 package com.example.android.contactsapp.viewmodel;
 
+import android.app.Application;
+import android.arch.lifecycle.AndroidViewModel;
+import android.arch.lifecycle.LifecycleOwner;
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
 import android.arch.lifecycle.ViewModel;
+import android.support.annotation.NonNull;
 
-public class ContactsViewModel extends ViewModel {
+import com.example.android.contactsapp.service.model.ContactListModel;
+
+public class ContactsViewModel extends AndroidViewModel {
+
+    private LiveData<ContactListModel> contactListModelLiveData;
+
+    public ContactsViewModel(@NonNull Application application, LiveData<ContactListModel> contactListModelLiveData){
+        super(application);
+        this.contactListModelLiveData =contactListModelLiveData;
+    }
+
+    public LiveData<ContactListModel> getContactListModel(){
+        return contactListModelLiveData;
+    }
 }
